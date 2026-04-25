9ee0dcbabbfef7a0defc7c16484712474468f67d
mahmoud10593 <mahmoud.abdelrahman10593@gmail.com>
2026-04-25 17:44:30 +0300

    .

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # ✅ CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # مؤقتًا عشان الاختبار
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
